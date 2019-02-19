d5d2a1ce97a01eb30f3c66d251c2b2552c633b5c
Zhao Xiaojie <LinuxSuRen@gmail.com>
2019-02-19 09:52:04 +0800

    Add translator infor in blog layout (#127)
    * add translator infor in blog layout
    
    * fixes wrong anchor

--- content/_ext/authorship.rb
+++ content/_ext/authorship.rb
@@ -42,4 +42,36 @@ module Authorship
 
     return node.author
   end
+
+  def display_translator_for(node, link = nil)
+    # bail early if what we were given doesn't even respond
+    return unless node.translator
+
+    translator = node.translator.to_sym
+
+    if node.translator && site.authors.has_key?(translator)
+      full_name = site.authors[translator].name
+
+      # If the caller provided a link use it, otherwise
+      if link.nil?
+        # Let's find a nice link to give our author
+        AUTHOR_LINK_PREFERENCE.each do |link_type, url|
+          value = site.authors[translator].send(link_type)
+          # If we didn't get anything, skip
+          next if value.nil?
+
+          link = url.gsub(/VALUE/, value.to_s)
+          break unless link.nil?
+        end
+
+        if link.nil?
+          return full_name
+        end
+      end
+
+      return "<a href=\"#{link}\" target=\"_blank\">#{full_name}</a>"
+    end
+
+    return node.translator
+  end
 end
